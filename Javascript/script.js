da3a1486341c25bce57707253195dc68738c267a
SamPetherick1898 <fuentessamuelignacio@gmail.com>
2022-06-18 20:04:27 -0500

    videjuego

---
+++ Javascript/script.js
@@ -0,0 +1,10 @@
+/* sonido del disparo de mi soldado
+
+const sonidos = document.getElementById("sonidos");
+
+document.addEventListener("keydown", function(evento){
+    if(evento.keyCode == 32){
+        sonidos.innerHTML = ''
+    }
+}) 
+*/
