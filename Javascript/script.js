8dfb73426e67985c2dfd3fe0dbf7125745688548
SamPetherick1898 <fuentessamuelignacio@gmail.com>
2022-06-20 15:30:13 -0500

    cambios al canvas

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,10 +1,39 @@
-/* sonido del disparo de mi soldado
+//Propiedas de movimiento de mi canvas
 
-const sonidos = document.getElementById("sonidos");
 
-document.addEventListener("keydown", function(evento){
-    if(evento.keyCode == 32){
-        sonidos.innerHTML = ''
+const img = new Image();
+img.src = '../Images/mapa2.jpeg';
+
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+const backgroundImage = {
+  img: img,
+  x: 0,
+  speed: -1,
+
+  move: function() {
+    this.x += this.speed;
+    this.x %= canvas.width;
+  },
+
+  draw: function() {
+    ctx.drawImage(this.img, this.x, 0);
+    if (this.speed < 0) {
+      ctx.drawImage(this.img, this.x + canvas.width, 0);
+    } else {
+      ctx.drawImage(this.img, this.x - this.img.width, 0);
     }
-}) 
-*/
+  },
+};
+
+function miCanvas() {
+  backgroundImage.move();
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  backgroundImage.draw();
+
+  requestAnimationFrame(updateCanvas);
+}
+
+img.onload = updateCanvas;
